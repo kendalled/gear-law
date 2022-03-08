dbe2da389ba29fd8972b94a593e2547c8e28ed20
Kendall Jackson <kendallkj00@gmail.com>
2022-03-08 10:28:12 -0500

    fix: unify cta button text

--- pages/contact.vue
+++ pages/contact.vue
@@ -38,7 +38,7 @@
             <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-600">Blog</a>
           </div>
           <div class="hidden lg:flex lg:items-center lg:space-x-6">
-            <a href="#" class="px-6 py-2 text-base font-medium text-blue-600 bg-white border border-transparent rounded-md shadow-md hover:bg-gray-50"> Contact </a>
+            <a href="#" class="px-6 py-2 text-base font-medium text-blue-600 bg-white border border-transparent rounded-md shadow-md hover:bg-gray-50"> Contact us </a>
           </div>
         </nav>
       </div>
