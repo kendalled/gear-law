bc36495068d2963f7c3bf349777bf4ddd37bb480
Kendall Jackson <kendallkj00@gmail.com>
2022-03-22 14:16:31 -0400

    fix: change order & copy of services

--- components/Services.vue
+++ components/Services.vue
@@ -11,7 +11,7 @@
             We provide a full array of affordable business law and bankruptcy law services to small and middle market businesses and entrepenuers. We are a full service law firm that provides a comprehensive range of services to help you achieve your goals.
           </p>
         </div>
-        <ServiceCard service1="Financial" service2="Bankruptcy" class="mt-6 lg:mt-0">
+        <ServiceCard service1="Personal" service2="Injury" class="mt-6 lg:mt-0">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="w-16 h-16"
@@ -26,12 +26,13 @@
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <line x1="3" y1="21" x2="21" y2="21" />
-            <path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" />
-            <line x1="5" y1="21" x2="5" y2="10.85" />
-            <line x1="19" y1="21" x2="19" y2="10.85" />
+            <path d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16" />
             <path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" />
+            <line x1="10" y1="9" x2="14" y2="9" />
+            <line x1="12" y1="7" x2="12" y2="11" />
           </svg>
         </ServiceCard>
+
         <ServiceCard service1="Family" service2="Law" class="mt-6">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -75,7 +76,7 @@
             <line x1="9" y1="18" x2="9" y2="18.01" />
           </svg>
         </ServiceCard>
-        <ServiceCard service1="Personal" service2="Injury" class="mt-6">
+        <ServiceCard service1="Business" service2="Law" class="mt-6">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="w-16 h-16"
@@ -90,12 +91,13 @@
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <line x1="3" y1="21" x2="21" y2="21" />
-            <path d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16" />
+            <path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" />
+            <line x1="5" y1="21" x2="5" y2="10.85" />
+            <line x1="19" y1="21" x2="19" y2="10.85" />
             <path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" />
-            <line x1="10" y1="9" x2="14" y2="9" />
-            <line x1="12" y1="7" x2="12" y2="11" />
           </svg>
         </ServiceCard>
+
         <!-- <div class="col-span-1 mt-8 bg-blue-800 rounded-sm shadow h-96 w-96" />
         <div class="col-span-1 mt-8 bg-blue-800 rounded-sm shadow h-96 w-96" />
         <div class="col-span-1 mt-8 bg-blue-800 rounded-sm shadow h-96 w-96" /> -->
