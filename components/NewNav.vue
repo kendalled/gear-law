11b7ead792edff7bb1a2297a2559873e652cb04f
Kendall Jackson <kendallkj00@gmail.com>
2022-05-25 17:08:11 -0400

    fix: add correct copy to mobile

--- components/NewNav.vue
+++ components/NewNav.vue
@@ -139,7 +139,7 @@
                     >
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                     </svg>
-                    <span class="ml-3 text-base font-medium text-gray-900"> Analytics </span>
+                    <span class="ml-3 text-base font-medium text-gray-900"> Personal Injury </span>
                   </a>
 
                   <a href="#" class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50">
@@ -154,7 +154,7 @@
                     >
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
                     </svg>
-                    <span class="ml-3 text-base font-medium text-gray-900"> Engagement </span>
+                    <span class="ml-3 text-base font-medium text-gray-900"> Business Law </span>
                   </a>
 
                   <a href="#" class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50">
@@ -169,7 +169,7 @@
                     >
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                     </svg>
-                    <span class="ml-3 text-base font-medium text-gray-900"> Security </span>
+                    <span class="ml-3 text-base font-medium text-gray-900"> Family Law </span>
                   </a>
 
                   <a href="#" class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50">
@@ -184,7 +184,7 @@
                     >
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                     </svg>
-                    <span class="ml-3 text-base font-medium text-gray-900"> Integrations </span>
+                    <span class="ml-3 text-base font-medium text-gray-900"> Criminal Law </span>
                   </a>
 
                   <a href="#" class="flex items-center p-3 -m-3 rounded-md hover:bg-gray-50">
@@ -221,7 +221,7 @@
                 <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700"> Security </a>
               </div>
               <div>
-                <a href="#" class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700"> Contact us </a>
+                <nuxt-link to="/contact" title="Contact form" class="flex items-center justify-center w-full px-4 py-2 text-base font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700"> Contact us </nuxt-link>
                 <p class="mt-6 text-base font-medium text-center text-gray-500">
                   Existing customer?
                   <a href="#" class="text-blue-600 hover:text-blue-500"> Sign in </a>
