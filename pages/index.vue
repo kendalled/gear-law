ea9c40ee0239d9143b635729ccbd57109fed864e
Kendall Jackson <kendallkj00@gmail.com>
2022-12-20 17:43:49 -0500

    fix: remove barry from page title/desc

--- pages/index.vue
+++ pages/index.vue
@@ -20,12 +20,12 @@
 export default {
   layout: 'default',
   head: {
-    title: 'Gagnon, Eisele & Rigby Attorneys: Full Service Orlando Law Firm',
+    title: 'Gagnon & Eisele Attorneys: Full Service Orlando Law Firm',
     meta: [
       {
         hid: 'description',
         name: 'description',
-        content: 'Gagnon, Eisele & Rigby Attorneys Serving Orlando & Central Florida for over 10 years. Hire us, and you\'re not hiring just a attorney but a team that collaborates, to achieve the absolute best out come for our clients cases.'
+        content: 'Gagnon, & Eisele Attorneys Serving Orlando & Central Florida for over 10 years. Hire us, and you\'re not hiring just a attorney but a team that collaborates, to achieve the absolute best out come for our clients cases.'
       }
     ]
   }
